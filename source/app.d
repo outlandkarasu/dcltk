188fca6dd099b5f9d06eed09f9f68b1848b99b37
outlandkarasu <outland.karasu@gmail.com>
2018-09-01 00:57:30 +0900

    add highest loading.

--- source/app.d
+++ source/app.d
@@ -3,17 +3,13 @@ import std.stdio : writefln;
 import cl = dcltk;
 
 void main() {
-    cl.loadOpenCl();
-
-    foreach(i, id; cl.getPlatformIds()) {
-        writefln("%d: %s %s %s(%s) %s [%s]",
-                i,
-                cl.getPlatformProfile(id),
-                cl.getPlatformName(id),
-                cl.getPlatformVersion(id),
-                cl.getPlatformCLVersion(id),
-                cl.getPlatformVendor(id),
-                cl.getPlatformExtensions(id));
-    }
+    auto platformId = cl.loadOpenCl();
+    writefln("loaded: %s %s %s(%s) %s [%s]",
+        cl.getPlatformProfile(platformId),
+        cl.getPlatformName(platformId),
+        cl.getPlatformVersion(platformId),
+        cl.getPlatformCLVersion(platformId),
+        cl.getPlatformVendor(platformId),
+        cl.getPlatformExtensions(platformId));
 }
 
