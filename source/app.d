1910978409977243aafd6c02497e074b4ed46b05
outlandkarasu <outland.karasu@gmail.com>
2018-09-01 00:45:13 +0900

    add platform version.

--- source/app.d
+++ source/app.d
@@ -6,11 +6,12 @@ void main() {
     cl.loadOpenCl();
 
     foreach(i, id; cl.getPlatformIds()) {
-        writefln("%d: %s %s %s %s %s",
+        writefln("%d: %s %s %s(%s) %s [%s]",
                 i,
                 cl.getPlatformProfile(id),
                 cl.getPlatformName(id),
                 cl.getPlatformVersion(id),
+                cl.getPlatformCLVersion(id),
                 cl.getPlatformVendor(id),
                 cl.getPlatformExtensions(id));
     }
